3e9da7200d7c19dc016ff3e16324203175964207
Laurie Pfeffer <pfefferlaurie@gmail.com>
2025-10-17 13:57:43 +0100

    coverage

--- frontend/src/setupTests.js
+++ frontend/src/setupTests.js
@@ -1,7 +1,2 @@
-// src/setupTests.js
-import '@testing-library/jest-dom'
-import matchers from '@testing-library/jest-dom/matchers'
-import { expect } from 'vitest'
 
-// Étend les matchers de jest-dom à Vitest
-expect.extend(matchers)
+import '@testing-library/jest-dom'
